ff26ecf0da53c7d4e409461a1794f4187c3b307f
Ad-128 <52488171+Ad-128@users.noreply.github.com>
2019-07-03 18:40:49 +1000

    First program

---
+++ beginner.py
@@ -0,0 +1,107 @@
+# Different ways to convert hours to minutes
+
+#Option 1
+#print(4*60)
+
+#Option 2
+
+#hours=4
+#minutes=4*60
+
+#print(minutes)
+
+#Option 3
+#hours=int(input('Enter a number'))
+#minutes=hours*60
+#print(minutes)
+
+# a='this is a string '
+# b = '*'
+# c='5'
+# d='6'
+# pattern=b+' '*3+b+' '*4+b
+#
+# '''
+# print(10*b)
+# for i in range(1,4):
+#     print(pattern)
+# print(10*b)
+# for i in range(1,4):
+#     print(pattern)
+#
+# print(10 * b)
+# '''
+# sum=0
+# for i in range(1,6):
+#     print(i)
+#     sum = sum+i
+#
+#print(sum)
+
+# number=230
+# for i in range(1,21):
+#     product=number*i
+#     print(product)
+
+# # name="basant kumar singh khati"
+# name="123456789"
+#
+# for i in range(0,len(name)):
+#     for j in range(-1,i):
+#         print(name[i], end="   ")
+#     print()
+#
+'''
+figure = "*"
+print()
+for i in range(0,6):
+    print(figure,end=" ")
+print()
+for i in range(0, 6):
+    print(figure+"         "+figure)
+for i in range(0, 6):
+    print(figure, end=" ")
+
+
+
+figure = "*"
+print()
+for j in range(0,6):
+    for i in range(0,6):
+        print(figure,end=" ")
+    print()
+'''
+
+space = ' '
+star = '*'
+
+pattern=4*space+star+4*space
+
+print("this is division")
+for i in range(0,1):
+    print(pattern)
+print(9*star)
+for i in range(0,1):
+    print(pattern)
+
+# this is pattern for subtraction
+print("this is subtraction")
+print(9 * star)
+
+# this is pattern for addition
+print("this is addition")
+
+for i in range(0,3):
+    print(pattern)
+print(9 * star)
+for i in range(0,3):
+    print(pattern)
+
+for i in range(0,4):
+    print(9*star)
+
+    pattern=9*star
+
+for i in range(0,4):
+    print(pattern)
+
