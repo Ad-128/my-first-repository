c835db0d502e817bb308e96d640e8947b26a4ec3
Ad-128 <52488171+Ad-128@users.noreply.github.com>
2019-07-03 19:17:12 +1000

    Create patterns.py

---
+++ patterns.py
@@ -0,0 +1,23 @@
+# This file has different patterns to print
+# Author : Ad-128
+# Date : 03/07/2019
+
+# Pattern-1
+# Print a pattern like this -
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+
+star='*'
+space=' '
+pattern=star+space
+print("*")
+print("* *")
+print("* * *")
+print("* * * *")
+
+for i in range(1,5):
+    print(i*pattern)
+
+
